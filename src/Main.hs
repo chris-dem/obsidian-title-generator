16cfdd71670982ff0b647585229fc320d1ca2170
chris-dem <christosdeme@gmail.com>
2025-06-27 10:55:05 +0100

    Stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,20 @@
 module Main (main) where
 
+import Control.Monad (forM_)
 import Data.Char (toLower)
-import Data.List (intercalate)
+import Data.List (intercalate, uncons)
 import Data.Time
+import System.Environment (getArgs)
+import Text.Regex.Posix
 
-extractArguments :: IO String
-extractArguments = getLine
+extractArguments :: IO (Maybe String)
+extractArguments = fmap fst . uncons <$> getArgs
+
+filterNonChars :: String -> String
+filterNonChars input = concat (getAllTextMatches (input =~ "[A-Za-z0-9]" :: AllTextMatches [] String))
 
 generateTitle :: Int -> String -> String
-generateTitle i = intercalate "-" . (show i :) . fmap (fmap toLower) . words
+generateTitle i = intercalate "-" . (show i :) . fmap (filterNonChars . fmap toLower) . words
 
 getDate :: IO Int
 getDate = read . formatTime defaultTimeLocale "%s" <$> getCurrentTime
@@ -16,4 +22,5 @@ getDate = read . formatTime defaultTimeLocale "%s" <$> getCurrentTime
 main :: IO ()
 main = do
   i <- getDate
-  extractArguments >>= putStrLn . generateTitle i
+  args <- extractArguments
+  forM_ args (putStrLn . generateTitle i)
